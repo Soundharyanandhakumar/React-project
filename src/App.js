b1dcee264ad4b898e0c479d340e975e20ec4727c
Soundharya Nandhakumar <soundharya.nandhakumar@SILTP-11063.local>
2023-05-23 15:08:41 +0530

    app creation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<nav class="navbar background">
+				<ul class="nav-list">
+					<li><a href="#Bundle creation">Bundle creation</a></li>
+					<li><a href="#Analytics and reporting">Analytics and reporting</a></li>
+					<li><a href="Data communication">Data communication</a></li>
+					<li><a href='#Documentation'>Documentation</a></li>
+				</ul>
+
+			</nav>
+
+					</div>
+	)
+}
+export default App
